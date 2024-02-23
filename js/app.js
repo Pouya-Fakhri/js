515cafd16c5ae54b1edcd49398e301a32e16bd8e
pouya <pouya.May4@gmail.com>
2024-02-24 02:39:53 +0330

    js is so good

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-let firstName = 'pouya';
-let lastName = 'fakhri';
-let age = 14;
-let contre = 'iran';
+let num1 = Number(prompt('enter a number'))
+let operator = prompt('what hapens')
+let num2 = Number(prompt('enter a number'))
+let risult
+if(operator == '+'){
+    risult = num1 + num2
+}
+else if(operator == '-'){
+    risult = num1 - num2
+}
+else if(operator == '*'){
+    risult = num1 * num2
+}
+else if(operator == '/'){
+    risult = num1 / num2
+}
+else if(operator == '**'){
+    risult = num1 ** num2
+}
+alert(risult)
